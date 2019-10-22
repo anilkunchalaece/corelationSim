28ec90762bcf8b280fd339682acdf0b72e67f9a1
anilkunchalaece <anilkunchalaece@gmail.com>
2019-10-22 16:20:29 +0100

    date range is not consistent need to create a new date time index

--- dataProcess.py
+++ dataProcess.py
@@ -9,15 +9,19 @@ Grand-St-Bernard Deployment
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 class Data :
     def __init__(self) :
         self.dataDir = os.path.join(os.getcwd(),'data')
 
 
-    def visualizeRandomStation(self):
+    def visualizeRandomStation(self,nodeNum=None):
         #this function will get function from random station and displays the graphs for all features
-        randomNodeNum = np.random.randint(len(os.listdir(self.dataDir)))
+        if nodeNum == None :
+            randomNodeNum = np.random.randint(len(os.listdir(self.dataDir)))
+        else :
+            randomNodeNum = nodeNum
         print("visualization of node num {0}".format(randomNodeNum))
         with open(os.path.join(self.dataDir,str(randomNodeNum)+'.txt')) as f:
             sensorData = f.readlines()
@@ -85,6 +89,43 @@ class Data :
         plt.ylim(ymin, ymax)
         print('ymin is {0} and y max is {1}'.format(ymin,ymax))
         plt.show()
+    
+    def upSampleData(self,fileName,samplingDuration) :
+        #this function will upsample data based on sampling duration
+        # duration will be in seconds ex 1S , 2S
+        data = []
+        with open(os.path.join(self.dataDir,fileName)) as f :
+            dateCols = [[1,2,3,4,5,6]]
+            parser = lambda date: pd.datetime.strptime(date, '%d%b%Y')
+            #nrows is used to limit the number of rows to read - we are reading only 10k rows since author only takes 10k in his publication
+            df = pd.read_csv(f,parse_dates=dateCols,sep=' ',header=None,nrows=10000)
+        df['date'] = pd.to_datetime(df['1_2_3_4_5_6'], format="%Y %m %d %H %M %S",)
+        # print(df['date'])
+        #remove date time colums to keep formatted date
+        del df['1_2_3_4_5_6']
+        del df[7]
+        del df[0]
+        del df[15]
+        del df[10]
+        del df[12]
+        del df[13]
+        del df[14]
+        df.set_index('date', inplace=True)
+        newdf = df.resample(samplingDuration).fillna('ffill').interpolate()
+        # ax = df.plot()
+        # ax.legend(['aTemp', 'sTemp','rHumi','wDirection'])
+        # plt.title('Sensor Data for {}'.format(fileName))
+        # plt.show()
+        print('original data frame shape is {0} after resamping its shape is {1}'.format(df.shape,newdf.shape))
+        # print(newdf.shape)
+        return newdf
+    
+    def upSampleDataAndSaveIt(self,directoryToSave,samplingDuration):
+        allFiles = os.listdir(self.dataDir)
+        for fileName in allFiles :
+            df = self.upSampleData(fileName,samplingDuration)
+            df.to_csv(os.path.join(os.getcwd(),directoryToSave,fileName))
+        
         
 
 def getDataBasedOnIndex(data,index) :
@@ -95,8 +136,10 @@ def getDataBasedOnIndex(data,index) :
 
 if __name__ == '__main__' :
     data = Data()
-    # data.visualizeRandomStation()
-    data.visualizeSingleFeature('aTemp')
+    # data.visualizeRandomStation(nodeNum=10)
+    # data.visualizeSingleFeature('aTemp')
+    # data.upSampleData('20.txt')
+    data.upSampleDataAndSaveIt('resampledData','1S')
 
 
         
